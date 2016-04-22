2e175463886f048f403b8564f0b29112959e9a1f
Timo <timo@teatwig.net>
2016-04-22 13:58:34 +0200

    NonogramNumbers cleanup

--- src/main/java/net/teatwig/mineswraft/NonogramNumbers.java
+++ src/main/java/net/teatwig/mineswraft/NonogramNumbers.java
@@ -1,14 +1,19 @@
 package net.teatwig.mineswraft;
 
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by timo on 15.04.2016.
@@ -17,118 +22,90 @@ class NonogramNumbers {
     private List<List<Integer>> colNumbers, rowNumbers;
 
     NonogramNumbers(Board board) {
-        colNumbers = new ArrayList<>();
-        rowNumbers = new ArrayList<>();
-
-        setColumnNumbers(board);
+        setColNumbers(board);
         setRowNumbers(board);
     }
 
-    private void setColumnNumbers(Board board) {
-        IntStream.range(0, board.getWidth()).forEach(no -> {
-            colNumbers.add(new ArrayList<>());
-            int currentNumber = 0;
-            for(Field f : board.getColumn(no)) {
-                if(f.isMine()) {
-                    currentNumber++;
-                } else if(!f.isMine()) {
-                    if(currentNumber>0)
-                        getLast(colNumbers).add(currentNumber);
-                    currentNumber = 0;
-                }
-            }
-            if(currentNumber>0) {
-                getLast(colNumbers).add(currentNumber);
-            }
-        });
+    private void setColNumbers(Board board) {
+        colNumbers = new ArrayList<>();
+        IntStream.range(0, board.getWidth()).forEach(no -> addNumbersToFromColOrRow(colNumbers, board.getColumn(no)));
     }
 
     private void setRowNumbers(Board board) {
-        IntStream.range(0, board.getWidth()).forEach(no -> {
-            rowNumbers.add(new ArrayList<>());
-            int currentNumber = 0;
-            for(Field f : board.getRow(no)) {
-                if(f.isMine()) {
+        rowNumbers = new ArrayList<>();
+        IntStream.range(0, board.getWidth()).forEach(no -> addNumbersToFromColOrRow(rowNumbers, board.getRow(no)));
+    }
+
+    private void addNumbersToFromColOrRow(List<List<Integer>> numbers, Field[] arr) {
+        numbers.add(new ArrayList<>());
+        int currentNumber = 0;
+        if(Arrays.stream(arr).noneMatch(Field::isMine)) {
+            getLast(numbers).add(0);
+        } else {
+            for (Field f : arr) {
+                if (f.isMine()) {
                     currentNumber++;
-                } else if(!f.isMine()) {
-                    if(currentNumber>0)
-                        getLast(rowNumbers).add(currentNumber);
+                } else if (!f.isMine()) {
+                    if (currentNumber > 0)
+                        getLast(numbers).add(currentNumber);
                     currentNumber = 0;
                 }
             }
-            if(currentNumber>0) {
-                getLast(rowNumbers).add(currentNumber);
+            if (currentNumber > 0) {
+                getLast(numbers).add(currentNumber);
             }
-        });
+        }
     }
 
     GridPane getColNumberAsGridPane() {
-        GridPane gridPane = new GridPane();
-        for(int i = 0; i< colNumbers.size(); i++) {
-            if(colNumbers.get(i).isEmpty()) {
-                Button b = new Button();
-                b.setVisible(false);
-                gridPane.add(b, i, 0);
-            }
-            for(int j = 0; j< colNumbers.get(i).size(); j++) {
-                int deltaSize = getXNumMaxSize() - colNumbers.get(i).size();
-                gridPane.add(new Button(colNumbers.get(i).get(j).toString()), i, j+deltaSize);
-            }
-        }
-        applyButtonStyles(gridPane, true);
-        return gridPane;
+        return getGridPaneFor(colNumbers, true);
     }
 
     GridPane getRowNumberAsGridPane() {
+        return getGridPaneFor(rowNumbers, false);
+    }
+
+    private GridPane getGridPaneFor(List<List<Integer>> numbers, boolean columns) {
         GridPane gridPane = new GridPane();
-        for(int i = 0; i< rowNumbers.size(); i++) {
-            if(rowNumbers.get(i).isEmpty()) {
-                Button b = new Button();
-                b.setVisible(false);
-                gridPane.add(b, 0, i);
-            }
-            for(int j = 0; j< rowNumbers.get(i).size(); j++) {
-                int deltaSize = getYNumMaxSize() - rowNumbers.get(i).size();
-                gridPane.add(new Button(rowNumbers.get(i).get(j).toString()), j+deltaSize, i);
-            }
-        }
-        applyButtonStyles(gridPane, false);
+        IntStream.range(0, numbers.size()).forEach(i -> {
+            if(columns)
+                gridPane.addColumn(i, getButtons(numbers, i, columns));
+            else
+                gridPane.addRow(i, getButtons(numbers, i, columns));
+        });
         return gridPane;
     }
 
-    private void applyButtonStyles(GridPane gridPane, boolean xNumbers) {
-        gridPane.getChildren().stream().forEach(child -> {
-            Button b = (Button) child;
+    private Node[] getButtons(List<List<Integer>> numbers, int i, boolean columns) {
+        Button[] buttons =  Stream.concat(
+            IntStream.range(0, getMaxSublistSize(numbers)-numbers.get(i).size()).mapToObj(emptyButton),
+            numbers.get(i).stream().map(String::valueOf).map(Button::new)
+        ).toArray(Button[]::new);
+
+        Arrays.stream(buttons).forEach(b -> {
             b.setPrefSize(Controller.BUTTON_SIZE, Controller.BUTTON_SIZE);
             b.setMinSize(Controller.BUTTON_SIZE, Controller.BUTTON_SIZE);
             b.setMaxSize(Controller.BUTTON_SIZE, Controller.BUTTON_SIZE);
             b.setPadding(Insets.EMPTY);
             b.setFont(Font.font(null, FontWeight.NORMAL, 16));
-            b.setStyle("-fx-border-style: "+(xNumbers?"hidden solid":"solid hidden")+
+            b.setStyle("-fx-border-style: "+(columns?"hidden solid":"solid hidden")+
                     "; -fx-border-color: #DDD; -fx-border-size: 1px; -fx-background-radius: 0; -fx-background-color: #EFEFEF");
-        });
-    }
-
-    private int getXNumMaxSize() {
-        return getSize(colNumbers);
-    }
-
-    private int getYNumMaxSize() {
-        return getSize(rowNumbers);
-    }
-
-    private <T> int getSize(List<List<T>> lists) {
-        return lists.stream().mapToInt(List::size).max().getAsInt();
-    }
-
-    private static Field[][] flipDiagonally(Field[][] fields) {
-        Field[][] flipFields = new Field[fields[0].length][fields.length];
-        for(int i=0; i<fields.length; i++) {
-            for(int j=0; j<fields[0].length; j++) {
-                flipFields[j][i] = fields[i][j];
+            if(b.getText().equals("0")) {
+                b.setTextFill(Color.valueOf("gray"));
             }
-        }
-        return flipFields;
+        });
+
+        return buttons;
+    }
+
+    private IntFunction<Button> emptyButton = i -> {
+        Button b = new Button();
+        b.setVisible(false);
+        return b;
+    };
+
+    private <T> int getMaxSublistSize(List<List<T>> lists) {
+        return lists.stream().mapToInt(List::size).max().getAsInt();
     }
 
     private static <T> T getLast(List<T> list) {
